7f065a4cf659b6c9765e6ca781c5efe3bdc77073
ayafushimi <aya.fushimi@gmail.com>
2017-02-07 12:28:12 -0500

    Done

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe, ing, amount) {
+  recipe[ing] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ing) {
+  delete recipe[ing];
+  return recipe;
+}
+
+function updateIngredient(recipe, ing, amount) {
+  recipe[ing] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var key in recipe) {
+    console.log(`this recipe calls for ${recipe[key]} of ${key}`);
+  }
+}
